c1ab2d3bed17d11312359c44b1113f9eaef2206c
paolo <paolo@corebox.co.uk>
2018-05-01 23:41:26 +0100

    added auto-accept

--- vpc-peering-dev-prod.tf
+++ vpc-peering-dev-prod.tf
@@ -1,9 +1,10 @@
  resource "aws_vpc_peering_connection" "dev-prod-vpc-peering" {
    peer_vpc_id   = "${aws_vpc.prod-vpc.id}"
    vpc_id        = "${aws_vpc.dev-vpc.id}"
+   auto_accept = true
 
    tags {
-     name = "dev-prod"
+     name = "dev-prod-peering"
    }
 
  }
